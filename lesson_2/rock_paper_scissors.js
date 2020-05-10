d1a820eaeee1b6ca0ace0d6c1565438d80f7dce8
Terence Yak <terenceyak@istox.com>
2020-05-10 20:44:12 +0800

    Refactor and allow short hand input

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,43 +1,88 @@
 const readline = require('readline-sync');
 const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 
+const WINNING_COMBOS = {
+  rock:     ['scissors', 'lizard'],
+  paper:    ['rock',     'spock'],
+  scissors: ['paper',    'lizard'],
+  lizard:   ['paper',    'spock'],
+  spock:    ['rock',     'scissors'],
+};
+
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function displayWinner(choice, computerChoice) {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+function decodeChoice(choice) {
+  switch (choice.toLowerCase()) {
+    case 'r' :
+      return 'rock';
+    case 'p' :
+      return 'paper';
+    case 'sc':
+      return 'scissors';
+    case 'l':
+      return 'lizard';
+    case 'sp':
+      return 'spock';
+    default:
+      return undefined;
+  }
+}
 
-  if ((choice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
-        (choice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
-        (choice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
-        (choice === 'spock' && (computerChoice === 'scissors' || computerChoice === 'rock')) ||
-        (choice === 'lizard' && (computerChoice === 'paper' || computerChoice === 'spock'))) {
-    prompt('You win!');
-  } else if ((choice === 'rock' && (computerChoice === 'paper' || computerChoice === 'spock')) ||
-               (choice === 'paper' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
-               (choice === 'scissors' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
-               (choice === 'spock' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
-               (choice === 'lizard' && (computerChoice === 'scissors' || computerChoice === 'rock'))) {
-    prompt('Computer wins!');
+function validateChoice(choice) {
+  if (choice === undefined) {
+    return false;
   } else {
-    prompt("It's a tie!");
+    return true;
+  }
+}
+
+function getOutcome(choice, computerChoice) {
+  if (WINNING_COMBOS[choice].includes(computerChoice)) {
+    return 'win';
+  } else if (choice === computerChoice) {
+    return 'tie';
+  } else {
+    return 'lose';
+  }
+}
+
+function displayChoice (choice, computerChoice) {
+  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+}
+
+function displayWinner(outcome) {
+  if (outcome === 'win') {
+    prompt("You win!");
+  } else if ( outcome === 'lose') {
+    prompt("Computer Wins!");
+  } else {
+    prompt("Its a tie!");
   }
 }
 
 while (true) {
   prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  prompt("Enter the short hand of your choice.");
+  prompt("r - rock, p - paper, sc - scissors, li - lizard, sp - spock");
   let choice = readline.question();
+  let playerChoice = decodeChoice(choice);
 
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
+  while (!validateChoice(playerChoice)) {
+    prompt("That's not a valid option, please choose a valid option");
     choice = readline.question();
+    playerChoice = decodeChoice(choice);
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  displayWinner(choice, computerChoice);
+  displayChoice(playerChoice, computerChoice);
+
+  let outcome = getOutcome(playerChoice, computerChoice);
+
+  displayWinner(outcome);
 
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
@@ -48,4 +93,3 @@ while (true) {
 
   if (answer[0] !== 'y') break;
 }
-
