dd9cdeaf0717e441d585fd9f61acec03911c8820
Terence Yak <terenceyak@tutanota.com>
2020-06-03 21:05:01 +0800

    Add bannerizer

--- small_problems/easy3/bannerizer.js
+++ small_problems/easy3/bannerizer.js
@@ -1,3 +1,13 @@
+logInBox("To boldly go where no one has gone before!");
 function logInBox(text) {
-
+    // edge
+    
+    const edge = '+-'.concat(Array(text.length).fill('-').join('')).concat('-+');
+    const space = '| '.concat(Array(text.length).fill(' ').join('')).concat(' |');
+    const textRow = '| '.concat(text).concat(' |');
+    console.log(edge);
+    console.log(space);
+    console.log(textRow);
+    console.log(space);
+    console.log(edge);
 }
